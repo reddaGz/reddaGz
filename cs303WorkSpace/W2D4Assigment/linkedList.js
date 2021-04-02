73d71a167f0a34023266184cdefd8d9fe61dd5c3
reddaGz <44gebre@gmail.com>
2021-04-01 20:28:56 -0500

    W2D4RecurtionPracticeAssigment

---
+++ cs303WorkSpace/W2D4Assigment/linkedList.js
@@ -0,0 +1,23 @@
+function ListNode(value,next){
+    this.value=value,
+    this.next=next
+    
+}
+function findListNode(node,target){
+    if(node.next===null)
+          return null
+    else if(node.value===target)
+       return node
+   
+   return findListNode(node.next,target)   
+}
+   
+   //Abe, Homer, Bart, Lisa, Maggie instead of a tree.
+    const maggie = new ListNode('Maggie',null);
+    const lisa = new ListNode('Lisa',maggie);
+    const bart = new ListNode('Bart',lisa);
+    const homer = new ListNode('Homer',bart); 
+    const abe = new ListNode('Abe',homer);
+    const head=new ListNode("head",abe)
+    console.log("print all the lists homer ")
+    console.log(findListNode(head,"Homer"))
---
+++ cs303WorkSpace/W2D4Assigment/tree.js
@@ -0,0 +1,88 @@
+
+function contains(root,target){
+    if(root.value===target){
+        return true;
+    }
+    else{
+        for(let value of root.descendents ){
+          return  contains(value,target)
+        }
+        return false
+    }
+}
+
+function findSubtree(root,target){
+    if(root.value===target){
+        return root;
+    }
+    else{
+        for(let value of root.descendents ){
+          return  findSubtree(value,target)
+        }
+        return 
+    }
+}
+function  treeModifier(root, modiferFunc){
+     if(root.value)
+         root.value= modiferFunc(root)
+        for(let node of root.descendents ){
+             treeModifier(node,modiferFunc)
+        }
+    }
+
+function allCaps(node){
+   return node.value.toUpperCase()
+}
+function addStar(node){
+    return "***"+node.value+"***"
+}
+function reverseValue(node){
+    let reverse=""
+    for(let i=node.value.length-1;i>=0;i--){
+        reverse+=node.value[i]
+    }
+    return reverse;
+}
+function treeCollector(root,collector=[]){
+    if(root.value)
+         collector.push(root.value)
+        for(let node of root.descendents ){
+            treeCollector(node,collector)
+        }
+        return collector
+}
+function TreeNode(value) { 
+    this.value = value;
+    this.descendents = []; 
+}
+
+    // create nodes with values
+    const abe = new TreeNode('Abe');
+    const homer = new TreeNode('Homer'); 
+    const bart = new TreeNode('Bart');
+    const lisa = new TreeNode('Lisa');
+    const maggie = new TreeNode('Maggie');
+     abe.descendents.push(homer); 
+    homer.descendents.push(bart, lisa, maggie);
+    //console.log(Object.entries(abe)) 
+    //console.log(abe.value)
+    //console.log("abe values"+abe)
+    // console.log(homer.descendents)
+    //console.log(abe.descendents[])
+    console.log("====================contains==============================")
+     console.log(contains(abe,"Bart"))//true
+     console.log(contains(abe,"Redda"))//false
+     console.log("====================findSubtree==============================")
+     console.log(findSubtree(abe,"Homer"))
+    console.log("==================treeModifier allCaps ================================")
+     treeModifier(abe,allCaps)
+     console.log(homer)//all must be capital after calling the tree modifier
+     console.log("==================treeModifier  addStar================================")
+     treeModifier(abe,addStar)
+     console.log(homer)//all must include ***value***
+     console.log("==================treeModifier reverseValue================================")
+     treeModifier(abe,reverseValue)
+     console.log(homer)
+     console.log("==================Tree collector================================")
+     console.log(treeCollector(abe))
+
