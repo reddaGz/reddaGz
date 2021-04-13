8fd1e0654cad094fdc5c8ad8023b6a1842934b30
reddaGz <44gebre@gmail.com>
2021-04-13 10:41:07 -0500

    W4D1Assigment

--- cs303WorkSpace/W4D1Assigment/BankAcount/bank.js
+++ cs303WorkSpace/W4D1Assigment/BankAcount/bank.js
@@ -4,25 +4,46 @@ const { Account } = require("./account");
 const { SavingsAccount } = require("./savingsaccount");
 const { CheckingAccount } = require("./checkingaccount");
 class Bank{
-    static nextNumber=0;//2
+    static nextNumber=0;
+    /**
+     * Bank constructor
+     */
     constructor(){
         Bank.nextNumber++;
         this. _account=[];
     }
+    /**
+     * add accounts
+     */
     addAccount(){
        this._account.push(new Account(Bank.nextNumber++))
     }
+    /**
+     * 
+     * @param {Number} interest is interest rate
+     */
     addSavingsAccount(interest){
         this._account.push(new SavingsAccount(Bank.nextNumber++,interest))
     }
+    /**
+     * 
+     * @param {Number} overdraft is over draft
+     */
     addCheckingAccount(overdraft){
         this._account.push(new CheckingAccount(Bank.nextNumber++,overdraft))
     }
-
+   /**
+    * 
+    * @param {Number} number  is account number
+    */
     closeAccount(number){
         let index=this._account.findIndex((item)=>item._number===number)
         this._account.splice(index,1)
     }
+    /**
+     * 
+     * @returns bank reports 
+     */
     accountReport(){
       let accountReport=""  
       for(let account of this._account ){
